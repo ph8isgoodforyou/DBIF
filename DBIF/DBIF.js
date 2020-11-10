0afef87bb222253c7686f8c75cb59dc10623c4a1
ph8isgoodforyou <ignasshallpass@gmail.com>
2020-11-10 19:26:09 +0200

    slider fix

--- DBIF/DBIF.js
+++ DBIF/DBIF.js
@@ -3,4 +3,4 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".intro-motto", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".intro-slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".hidden-intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1 });
